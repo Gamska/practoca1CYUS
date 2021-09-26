d66ced298e9dc624d927204b147c09a60e58168b
Gamska <55729790+Gamska@users.noreply.github.com>
2021-09-26 18:23:51 -0500

    Squashed commit of the following:
    commit 8488ce1476205a34ee7c632d308f5ddfead141c3
    Author: Gamska <55729790+Gamska@users.noreply.github.com>
    Date:   Sun Sep 26 18:22:28 2021 -0500
    
        V.02 CRUD EMPLEADOS
    
        se realizaron correctamente las evaluaciones y no presenta ningun error
    
    commit bc4fc8f09b2e2123c81ab989ff8e6ddab5ff89fa
    Author: Gamska <55729790+Gamska@users.noreply.github.com>
    Date:   Sun Sep 26 18:05:57 2021 -0500
    
        CRUD: Insertar Empleado
    
        realizamos la accion de insertar Empleado
    
    commit e3ef086ec18ba23dd7fd401a7c15ede4d6b0a060
    Author: Gamska <55729790+Gamska@users.noreply.github.com>
    Date:   Sun Sep 26 17:57:16 2021 -0500
    
        CRUD: Vista Empleados
    
        Podemos observar una tabla donde se pueden visualizar todos los  Empleados

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,25 +1,165 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Table,
+  Button,
+  Container,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  ModalFooter,
+} from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  { id: 1, nombre: "Cano Ku David", correo: "gamska@gmail.com",fecha: "18 de enero del 2000" },
+  { id: 2, nombre: "Rubio Urtecho", correo: "rubiourtecho@gmail.com",fecha: "18 de marzo del 2001" },
+];
+
+
+class App extends React.Component {
+
+  state = {
+    data: data,
+    modalActualizar: false,
+    modalInsertar: false,
+    form: {
+      id: "",
+      nombre: "",
+      correo: "",
+      fecha: "",
+    },
+  };
+//mostrar modal de insertar 
+mostrarModalInsertar = () => {
+  this.setState({
+    modalInsertar: true,
+  });
+};
+cerrarModalInsertar = () => {
+  this.setState({ modalInsertar: false });
+};
+//insertar empleado
+insertar= ()=>{
+  var valorNuevo= {...this.state.form};
+  valorNuevo.id=this.state.data.length+1;
+  var lista= this.state.data;
+  lista.push(valorNuevo);
+  this.setState({ modalInsertar: false, data: lista });
+}
+
+handleChange = (e) => {
+  this.setState({
+    form: {
+      ...this.state.form,
+      [e.target.name]: e.target.value,
+    },
+  });
+};
+
+  render() {
+    return (
+      <>
+      <Container>
+        <br />
+        <h1>GESTION DE EMPLEADOS V.02</h1>
+          <br />
+          <Button color="success" onClick={()=>this.mostrarModalInsertar()}>Crear</Button>
+          <br />
+          <Table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Correo</th>
+                <th>Fecha de Ingreso</th>
+                <th>Acción</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {this.state.data.map((dato) => (
+                <tr key={dato.id}>
+                  <td>{dato.id}</td>
+                  <td>{dato.nombre}</td>
+                  <td>{dato.correo}</td>
+                  <td>{dato.fecha}</td>
+                  <td>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
+        <Modal isOpen={this.state.modalInsertar}>
+          <ModalHeader>
+           <div><h3>Insertar Nuevo Empleado</h3></div>
+          </ModalHeader>
+
+          <ModalBody>
+            <FormGroup>
+              <label>Id: </label>
+              <input className="form-control" readOnly type="text" value={this.state.data.length+1}/>
+            </FormGroup>
+            <FormGroup>
+              <label>
+                Nombre: 
+              </label>
+              <input
+                className="form-control"
+                name="nombre"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Correo: 
+              </label>
+              <input
+                className="form-control"
+                name="correo"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Fecha de Ingreso: 
+              </label>
+              <input
+                className="form-control"
+                name="fecha"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.insertar()}
+            >
+              Insertar
+            </Button>
+            <Button
+              className="btn btn-danger"
+              onClick={() => this.cerrarModalInsertar()}
+            >
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </Modal>
+     
+
+</>
+    );
+  };
 }
 
 export default App;
