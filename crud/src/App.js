8488ce1476205a34ee7c632d308f5ddfead141c3
Gamska <55729790+Gamska@users.noreply.github.com>
2021-09-26 18:22:28 -0500

    V.02 CRUD EMPLEADOS
    se realizaron correctamente las evaluaciones y no presenta ningun error

--- crud/src/App.js
+++ crud/src/App.js
@@ -64,7 +64,7 @@ handleChange = (e) => {
       <>
       <Container>
         <br />
-        <h1>GESTION DE EMPLEADOS</h1>
+        <h1>GESTION DE EMPLEADOS V.02</h1>
           <br />
           <Button color="success" onClick={()=>this.mostrarModalInsertar()}>Crear</Button>
           <br />
