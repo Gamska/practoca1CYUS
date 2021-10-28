54f44fd527ad25a3960876f6c352f94cb1f7e879
Gamska <55729790+Gamska@users.noreply.github.com>
2021-10-28 17:33:27 -0500

    Actualizacion
    Consumo de API_CRUD

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,280 +1,177 @@
-import React from "react";
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  Table,
-  Button,
-  Container,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  FormGroup,
-  ModalFooter,
-} from "reactstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
-const data = [
-  { id: 1, nombre: "Cano Ku David", correo: "gamska@gmail.com",fecha: "18 de enero del 2000" },
-  { id: 2, nombre: "Rubio Urtecho", correo: "rubiourtecho@gmail.com",fecha: "18 de marzo del 2001" },
-];
+const url="http://localhost:3000/";
 
-
-class App extends React.Component {
-
-  state = {
-    data: data,
-    modalActualizar: false,
-    modalInsertar: false,
-    form: {
-      id: "",
-      nombre: "",
-      correo: "",
-      fecha: "",
-    },
-  };
-//mostrar modal de insertar 
-mostrarModalInsertar = () => {
-  this.setState({
-    modalInsertar: true,
-  });
-};
-cerrarModalInsertar = () => {
-  this.setState({ modalInsertar: false });
-};
-//mostrar el modal de  editar 
-mostrarModalActualizar = (dato) => {
-  this.setState({
-    form: dato,
-    modalActualizar: true,
-  });
-};
-
-cerrarModalActualizar = () => {
-  this.setState({ modalActualizar: false });
-};
-//mandar valores del editar 
-editar = (dato) => {
-  var contador = 0;
-  var arreglo = this.state.data;
-  arreglo.map((registro) => {
-    if (dato.id == registro.id) {
-      arreglo[contador].nombre = dato.nombre;
-      arreglo[contador].correo = dato.correo;
-      arreglo[contador].fecha = dato.fecha;
-    }
-    contador++;
-  });
-  this.setState({ data: arreglo, modalActualizar: false });
-};
-//insertar empleado
-insertar= ()=>{
-  var valorNuevo= {...this.state.form};
-  valorNuevo.id=this.state.data.length+1;
-  var lista= this.state.data;
-  lista.push(valorNuevo);
-  this.setState({ modalInsertar: false, data: lista });
-}
-//eliminar empleado
-eliminar = (dato) => {
-  var opcion = window.confirm("Estás Seguro que deseas Eliminar el elemento "+dato.id);
-  if (opcion == true) {
-    var contador = 0;
-    var arreglo = this.state.data;
-    arreglo.map((registro) => {
-      if (dato.id == registro.id) {
-        arreglo.splice(contador, 1);
-      }
-      contador++;
-    });
-    this.setState({ data: arreglo, modalActualizar: false });
+class App extends Component {
+state={
+  data:[],
+  modalInsertar: false,
+  modalEliminar: false,
+  form:{
+    id_empleado: '',
+    nombre: '',
+    email: '',
+    into_fecha: '',
+    tipoModal: ''
   }
-};
-
-handleChange = (e) => {
-  this.setState({
-    form: {
-      ...this.state.form,
-      [e.target.name]: e.target.value,
-    },
-  });
-};
-
-  render() {
-    return (
-      <>
-      <Container>
-        <br />
-        <h1>GESTION DE EMPLEADOS FINAL</h1>
-          <br />
-          <Button color="success" onClick={()=>this.mostrarModalInsertar()}>Crear</Button>
-          <br />
-          <Table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Correo</th>
-                <th>Fecha de Ingreso</th>
-                <th>Acción</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {this.state.data.map((dato) => (
-                <tr key={dato.id}>
-                  <td>{dato.id}</td>
-                  <td>{dato.nombre}</td>
-                  <td>{dato.correo}</td>
-                  <td>{dato.fecha}</td>
-                  <td>
-                  <Button color="primary" onClick={() => this.mostrarModalActualizar(dato)}>Editar </Button>{" "}
-                    <Button color="danger" onClick={()=> this.eliminar(dato)}>Eliminar</Button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </Container>
-        <Modal isOpen={this.state.modalInsertar}>
-          <ModalHeader>
-           <div><h3>Insertar Nuevo Empleado</h3></div>
-          </ModalHeader>
-
-          <ModalBody>
-            <FormGroup>
-              <label>Id: </label>
-              <input className="form-control" readOnly type="text" value={this.state.data.length+1}/>
-            </FormGroup>
-            <FormGroup>
-              <label>
-                Nombre: 
-              </label>
-              <input
-                className="form-control"
-                name="nombre"
-                type="text"
-                onChange={this.handleChange}
-              />
-            </FormGroup>
-            
-            <FormGroup>
-              <label>
-                Correo: 
-              </label>
-              <input
-                className="form-control"
-                name="correo"
-                type="text"
-                onChange={this.handleChange}
-              />
-            </FormGroup>
-            
-            <FormGroup>
-              <label>
-                Fecha de Ingreso: 
-              </label>
-              <input
-                className="form-control"
-                name="fecha"
-                type="text"
-                onChange={this.handleChange}
-              />
-            </FormGroup>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button
-              color="primary"
-              onClick={() => this.insertar()}
-            >
-              Insertar
-            </Button>
-            <Button
-              className="btn btn-danger"
-              onClick={() => this.cerrarModalInsertar()}
-            >
-              Cancelar
-            </Button>
-          </ModalFooter>
-        </Modal>
-     
-        <Modal isOpen={this.state.modalActualizar}>
-          <ModalHeader>
-           <div><h3>Editar Registro</h3></div>
-          </ModalHeader>
-
-          <ModalBody>
-            <FormGroup>
-              <label>
-               Id:
-              </label>
-            
-              <input
-                className="form-control"
-                readOnly
-                type="text"
-                value={this.state.form.id}
-              />
-            </FormGroup>
-            
-            <FormGroup>
-              <label>
-                Nombre: 
-              </label>
-              <input
-                className="form-control"
-                name="nombre"
-                type="text"
-                onChange={this.handleChange}
-                value={this.state.form.nombre}
-              />
-            </FormGroup>
-            
-            <FormGroup>
-              <label>
-                Correo: 
-              </label>
-              <input
-                className="form-control"
-                name="correo"
-                type="text"
-                onChange={this.handleChange}
-                value={this.state.form.correo}
-              />
-            </FormGroup>
-            <FormGroup>
-              <label>
-                Fecha de Ingreso: 
-              </label>
-              <input
-                className="form-control"
-                name="fecha"
-                type="text"
-                onChange={this.handleChange}
-                value={this.state.form.fecha}
-              />
-            </FormGroup>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button
-              color="primary"
-              onClick={() => this.editar(this.state.form)}
-            >
-              Editar
-            </Button>
-            <Button
-              color="danger"
-              onClick={() => this.cerrarModalActualizar()}
-            >
-              Cancelar
-            </Button>
-          </ModalFooter>
-        </Modal>
-
-</>
-    );
-  };
 }
 
+peticionGet=()=>{
+axios.get(url).then(response=>{
+  this.setState({data: response.data});
+}).catch(error=>{
+  console.log(error.message);
+})
+}
+
+peticionPost=async()=>{
+  delete this.state.form.id;
+ await axios.post(url,this.state.form).then(response=>{
+    this.modalInsertar();
+    this.peticionGet();
+  }).catch(error=>{
+    console.log(error.message);
+  })
+}
+
+peticionPut=()=>{
+  axios.put(url+this.state.form.id_empleado, this.state.form).then(response=>{
+    this.modalInsertar();
+    this.peticionGet();
+  })
+}
+
+peticionDelete=()=>{
+  axios.delete(url+this.state.form.id_empleado).then(response=>{
+    this.setState({modalEliminar: false});
+    this.peticionGet();
+  })
+}
+
+modalInsertar=()=>{
+  this.setState({modalInsertar: !this.state.modalInsertar});
+}
+
+seleccionarEmpresa=(empleado)=>{
+  this.setState({
+    tipoModal: 'actualizar',
+    form: {
+      id_empleado: empleado.id_empleado,
+      nombre: empleado.nombre,
+      email: empleado.email,
+      into_fecha: empleado.into_fecha
+    }
+  })
+}
+
+handleChange=async e=>{
+e.persist();
+await this.setState({
+  form:{
+    ...this.state.form,
+    [e.target.name]: e.target.value
+  }
+});
+console.log(this.state.form);
+}
+
+  componentDidMount() {
+    this.peticionGet();
+  }
+  
+
+  render(){
+    const {form}=this.state;
+  return (
+    <div className="App">
+    <br /><br /><br />
+  <button className="btn btn-success" onClick={()=>{this.setState({form: null, tipoModal: 'insertar'}); this.modalInsertar()}}>Agregar Empleado</button>
+  <br /><br />
+    <table className="table ">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nombre</th>
+          <th>Email</th>
+          <th>Fecha de Ingreso</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        {this.state.data.map(empleado=>{
+          return(
+            <tr>
+          <td>{empleado.id_empleado}</td>
+          <td>{empleado.nombre}</td>
+          <td>{empleado.email}</td>
+          <td>{empleado.into_fecha}</td>
+          <td>
+                <button className="btn btn-primary" onClick={()=>{this.seleccionarEmpresa(empleado); this.modalInsertar()}}><FontAwesomeIcon icon={faEdit}/></button>
+                {"   "}
+                <button className="btn btn-danger" onClick={()=>{this.seleccionarEmpresa(empleado); this.setState({modalEliminar: true})}}><FontAwesomeIcon icon={faTrashAlt}/></button>
+                </td>
+          </tr>
+          )
+        })}
+      </tbody>
+    </table>
+
+
+
+    <Modal isOpen={this.state.modalInsertar}>
+                <ModalHeader style={{display: 'block'}}>
+                  <span style={{float: 'right'}} onClick={()=>this.modalInsertar()}>x</span>
+                </ModalHeader>
+                <ModalBody>
+                  <div className="form-group">
+                    <label htmlFor="id">ID</label>
+                    <input className="form-control" type="text" name="id" id="id" readOnly onChange={this.handleChange} value={form?form.id_empleado:''}/>
+                    <br />
+                    <label htmlFor="nombre">Nombre</label>
+                    <input className="form-control" type="text" name="nombre" id="nombre" onChange={this.handleChange} value={form?form.nombre: ''}/>
+                    <br />
+                    <label htmlFor="nombre">Email</label>
+                    <input className="form-control" type="text" name="email" id="email" onChange={this.handleChange} value={form?form.email: ''}/>
+                    <br />
+                    <label htmlFor="capital_bursatil">Fecha de ingreso</label>
+                    <input className="form-control" type="text" name="into_fecha" id="into_fecha" onChange={this.handleChange} value={form?form.into_fecha:''}/>
+                  </div>
+                </ModalBody>
+
+                <ModalFooter>
+                  {this.state.tipoModal=='insertar'?
+                    <button className="btn btn-success" onClick={()=>this.peticionPost()}>
+                    Insertar
+                  </button>: <button className="btn btn-primary" onClick={()=>this.peticionPut()}>
+                    Actualizar
+                  </button>
+  }
+                    <button className="btn btn-danger" onClick={()=>this.modalInsertar()}>Cancelar</button>
+                </ModalFooter>
+          </Modal>
+
+
+          <Modal isOpen={this.state.modalEliminar}>
+            <ModalBody>
+               Estás seguro que deseas eliminar al empleado {form && form.nombre}
+            </ModalBody>
+            <ModalFooter>
+              <button className="btn btn-danger" onClick={()=>this.peticionDelete()}>Sí</button>
+              <button className="btn btn-secundary" onClick={()=>this.setState({modalEliminar: false})}>No</button>
+            </ModalFooter>
+          </Modal>
+  </div>
+
+
+
+  );
+}
+}
 export default App;
