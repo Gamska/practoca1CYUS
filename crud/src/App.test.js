ec8e7e8cabb2ca72915cc25c07facb6efca13c0d
Gamska <55729790+Gamska@users.noreply.github.com>
2021-11-01 21:40:43 -0600

    Testeo CRUD
    Testeamos el CRUD entero

--- crud/src/App.test.js
+++ crud/src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import axios from "axios";
+const url="http://localhost:3000/";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+test('Obtener Datos',()=>{
+  axios.get(url).then(response=>{
+    this.setState({data: response.data});
+  }).catch(error=>{
+    console.log(error.message);
+  })
+  });
